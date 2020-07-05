5913f95697fea25fb5aee5538e3d67a8b664330f
KumananNesa <kumanan.nesarajah@etu.u-pec.fr>
2020-07-05 23:22:07 +0200

    Affichage du hello word dans le navigateur

--- src/main/java/ServerMain/Appvert.java
+++ src/main/java/ServerMain/Appvert.java
@@ -4,22 +4,23 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
-@SuppressWarnings("deprecation") // A changer avec un import recent 
+@SuppressWarnings("deprecation") // A changer avec un import recent
 public class Appvert extends AbstractVerticle {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(Appvert.class); //
 	// Quand le verticle se lance
-	
-	
+
 	@Override
 	public void start() throws Exception {
-		
-		LOGGER.info("Dans le stop...");
+
+		LOGGER.info("Dans le start...");
+		vertx.createHttpServer().requestHandler(routingContext -> routingContext.response().end("Hello World!"))
+				.listen(8080);
 	}
 
 	// Quand le verticle s'arrête
 	@Override
 	public void stop() throws Exception {
-		 LOGGER.info("Dans le stop...");
+		LOGGER.info("Dans le stop...");
 	}
 }
