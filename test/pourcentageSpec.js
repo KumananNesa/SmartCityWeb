931f4506e8b18e0fe02f5ed7684614f0f9798c33
KumananNesa <kumanan.nesarajah@etu.u-pec.fr>
2020-08-02 23:53:57 +0200

    Test1

---
+++ test/pourcentageSpec.js
@@ -0,0 +1,13 @@
+describe ('pourcentage', function(){
+
+
+    describe('#evolution', function(){
+        
+        var a = 3;
+
+        assert.equal( a * 2, 6,'la multiplication n \'a pas fonctionner' )
+        
+    })
+
+
+})
