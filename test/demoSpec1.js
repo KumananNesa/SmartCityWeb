c2d04f5bdc08c4b015cbe5d20ced5daf9256ef01
KumananNesa <kumanan.nesarajah@etu.u-pec.fr>
2020-08-02 23:53:04 +0200

    Debut test

---
+++ test/demoSpec1.js
@@ -0,0 +1,15 @@
+var assert = require( 'assert');
+
+describe ('test de text', function(){
+
+
+    it('should do something', function(){
+        
+        var a = 3;
+
+        assert.equal( a * 2, 6,'la multiplication n \'a pas fonctionner' )
+        
+    })
+
+
+})
